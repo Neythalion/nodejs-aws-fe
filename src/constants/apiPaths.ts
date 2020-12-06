012ab42a0258ec2827d637a5d76583b9f985465f
Maksim_Nikishin <Maksim_Nikishin@epam.com>
2020-12-07 01:32:24 +0400

    no message

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,7 @@
 const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://n2fgw3znbb.execute-api.eu-west-1.amazonaws.com/dev/import',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
 const BASE_API_PATH = 'https://xyxr3kd5v4.execute-api.eu-west-1.amazonaws.com/dev';
