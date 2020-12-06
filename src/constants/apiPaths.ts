54c02770688b06e3695cb59299fce6137b26d0a8
Maksim_Nikishin <Maksim_Nikishin@epam.com>
2020-12-07 01:29:36 +0400

    Task 8 updated api paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,19 @@
 const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://n2fgw3znbb.execute-api.eu-west-1.amazonaws.com/dev/import',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://neythalion-aws-cart-api.eu-west-1.elasticbeanstalk.com/api',
+};
+
+const BASE_API_PATH = 'https://xyxr3kd5v4.execute-api.eu-west-1.amazonaws.com/dev';
+const PRODUCTS_API_PATHS = {
+    getProducts: `${BASE_API_PATH}/products`,
+    getProductById: `${BASE_API_PATH}/product`,
+    addProduct: `${BASE_API_PATH}/product`
 };
 
 export default API_PATHS;
+
+export {
+  PRODUCTS_API_PATHS
+}
